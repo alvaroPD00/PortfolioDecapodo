c1381fe805547b5f64bf734282e0d5832503980a
alvaroPerezDominguez <alvaro.pd00@gmail.com>
2025-09-04 19:29:51 -0300

    cambios hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -32,7 +32,7 @@ export default function HeroSection() {
       <div className="absolute inset-0 bg-gradient-to-br from-duke-blue/40 via-indigo/40 to-electric-violet/40">
         <div className="absolute inset-0 bg-[url('/abstract-gaming-background.png')] bg-cover bg-center opacity-40"></div>
       </div>
-      <div className="absolute inset-0 bg-electric-violet/40"></div>
+      <div className="absolute inset-0 bg-[#9933FF]/60"></div>
 
       {/* Content */}
       <div
@@ -41,26 +41,26 @@ export default function HeroSection() {
         }`}
       >
         <div className="mb-8">
-          <h1 className="text-6xl mb-4 bg-gradient-to-r from-white to-light-sky-blue bg-clip-text text-transparent font-pear-soda">
+          <h1 className="text-6xl mb-4  text-white font-pear-soda">
             Decápodo
           </h1>
-          <div className="w-32 h-32 mx-auto bg-gradient-to-br from-light-sky-blue to-columbia-blue rounded-full flex items-center justify-center mb-6 shadow-2xl">
+          <div className="mx-auto flex items-center justify-center mb-6">
             <Image
               src="/images/decapodo-logo.png"
               alt="Decápodo Logo"
               width={80}
               height={80}
-              className="w-24 h-24"
+              className="w-48 h-48"
             />
           </div>
           <h1 className="text-6xl mb-6 text-white font-pear-soda">
             Game Design
           </h1>
         </div>
-        <h3 className="text-xl md:text-2xl mb-4">
+        <h3 className="text-xl md:text-2xl  text-white mb-4">
           Transformo ideas creativas en experiencias jugables.
         </h3>
-        <p className="text-lg md:text-xl text-light-sky-blue max-w-2xl mx-auto mb-8">
+        <p className="text-lg md:text-xl text-white max-w-2xl mx-auto mb-8">
           Especializado en narrativa, planificación estética e implementación en
           Unity.
         </p>
